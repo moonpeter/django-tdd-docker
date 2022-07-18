10385c1fcf485d07fcd9a3f3994457fcd6779901
moonpeter <moonpeter@kakao.com>
2022-07-18 20:22:24 +0900

    Django Rest Framework

--- app/conftest.py
+++ app/conftest.py
@@ -1,20 +1,20 @@
-import os
+# import os
 
-from django.conf import settings
+# from django.conf import settings
 
-import pytest
-
-
-DEFAULLT_ENGINE = "django.db.backends.postgresql_psycopg2"
+# import pytest
 
 
-@pytest.fixture(scope="session")
-def django_db_setup():
-    settings.DATABASES["default"] = {
-        "ENGINE": os.environ.get("DB_TEST_ENGINE", DEFAULLT_ENGINE),
-        "HOST": os.environ["DB_TEST_HOST"],
-        "NAME": os.environ["DB_TEST_NAME"],
-        "PORT": os.environ["DB_TEST_PORT"],
-        "USER": os.environ["DB_TEST_USER"],
-        "PASSWORD": os.environ["DB_TEST_PASSWORD"],
-    }
+# DEFAULLT_ENGINE = "django.db.backends.postgresql_psycopg2"
+
+
+# @pytest.fixture(scope="session")
+# def django_db_setup():
+#     settings.DATABASES["default"] = {
+#         "ENGINE": os.environ.get("DB_TEST_ENGINE", DEFAULLT_ENGINE),
+#         "HOST": os.environ["DB_TEST_HOST"],
+#         "NAME": os.environ["DB_TEST_NAME"],
+#         "PORT": os.environ["DB_TEST_PORT"],
+#         "USER": os.environ["DB_TEST_USER"],
+#         "PASSWORD": os.environ["DB_TEST_PASSWORD"],
+#     }
